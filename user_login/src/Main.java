653bf615eb1b04b0d0eb34c890c0dfcc496749a9
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-01-12 22:17:06 +0300

    user_pass_check

--- user_login/src/Main.java
+++ user_login/src/Main.java
@@ -2,10 +2,11 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String username;
         String password;
+        String reset;
+
 
         String[] users = {"berk", "esra"};
         String[] passwords = {"berk123", "esra123"};
@@ -15,14 +16,28 @@ public class Main {
         username = scanner.nextLine();
         System.out.println("Your password: ");
         password = scanner.nextLine();
-        scanner.close();
+
 
         if (Arrays.asList(users).contains(username) && Arrays.asList(passwords).contains(password)) {
             System.out.println("access granted");
+        }
+        else if (!Arrays.asList(passwords).contains(password)){
+            System.out.println("your pass is incorrect. Please change it...");
+
+            System.out.println("Enter your new pass: ");
+            reset = scanner.nextLine();
+
+            scanner.close();
+
+            if (Arrays.asList(passwords).contains(reset)) {
+                System.out.println("new password cannot be the same.");
+            }
+            else {
+                System.out.println("password changed.");
+            }
         }
         else {
             System.out.println("blocked");
         }
     }
 }
-
