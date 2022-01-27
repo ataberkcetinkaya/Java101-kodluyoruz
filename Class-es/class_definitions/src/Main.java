c34331a531aa3aa6df67964c2050db9690576c0e
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-01-27 17:21:56 +0300

    trying_in_main

--- Class-es/class_definitions/src/Main.java
+++ Class-es/class_definitions/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Car car = new Car();
+        car.increaseSpeed(10);
+        car.printSpeed();
     }
 }
