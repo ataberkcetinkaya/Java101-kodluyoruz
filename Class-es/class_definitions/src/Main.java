e0bcf715c23321f3cc8baac9d10116de8208d525
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-01-27 17:49:16 +0300

    usage_in_main

--- Class-es/class_definitions/src/Main.java
+++ Class-es/class_definitions/src/Main.java
@@ -5,5 +5,16 @@ public class Main {
         Car car = new Car();
         car.increaseSpeed(10);
         car.printSpeed();
+
+        //nesne olusturma dersi
+//        Car audi = new Car();
+//        audi.model = "A3 Sedan";
+//        audi.speed = 10;
+//            System.out.println(audi.model + " Speed: " + audi.speed);
+//
+//        Car bmw = new Car();
+//        bmw.model = "F10 M5";
+//        bmw.speed = 20;
+//            System.out.println(bmw.model + " Speed: " + bmw.speed);        
     }
 }
